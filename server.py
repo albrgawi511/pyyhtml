148a896c821fc2c8e8d26aeef6945da6f2fa9008
naif <naifmna7ii@gmail.com>
2025-04-27 08:23:48 +0300

    تحديث server.py لدعم البورت المتغير

--- server.py
+++ server.py
@@ -15,5 +15,8 @@ def upload():
     requests.post(url, files=files, data=data)
     return 'تم الاستلام'
 
+import os
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
